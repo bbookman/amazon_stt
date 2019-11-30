8764ef3efe293e4a4b2d7e04d8b7d08b64667c09
Bruce Bookman <bruce.bookman@gmail.com>
2019-11-29 17:04:09 -0800

    works

---
+++ run.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+
+import time, json
+import boto3, pdb
+
+"""
+s3 = boto3.resource('s3')
+obj = s3.Object(bucketname, itemname)
+body = obj.get()['Body'].read() """
+
+transcribe = boto3.client('transcribe')
+bucket = 'mytestbucketbookman'
+
+job_name = "7"  #must be unique or removed?
+job_uri = f"s3://{bucket}/test.wav"
+
+transcribe.start_transcription_job(
+    TranscriptionJobName=job_name,
+    Media={'MediaFileUri': job_uri},
+    MediaFormat='wav',
+    LanguageCode='en-US',
+    OutputBucketName=bucket
+
+)
+
+while True:
+    status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+    if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+        break
+    print(f"{status['TranscriptionJob']['TranscriptionJobStatus']}")
+    time.sleep(5)
+transcript_url = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
+print(f"GOT TRANSCRIPT URL: {transcript_url}")
+
+# get json
+itemname = job_name + ".json"
+
+s3 = boto3.resource('s3')
+obj = s3.Object(bucket, itemname)
+body = obj.get()['Body'].read()
+data = json.loads(body)
+print(data['results']['transcripts'][0]['transcript'])
+# save output as text
